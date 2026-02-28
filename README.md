3e5daaa2a57d38bf72d76c561f366f460365339e
J-Karthikeyan <jkarthik2611@gmail.com>
2026-03-01 01:34:15 +0530

    dotfiles

---
+++ README.md
@@ -0,0 +1,8 @@
+# Metal Theme Dotfiles
+
+- ghostty
+- zsh (oh-my-zsh, starship, antidote)
+- nvim (lazy.nvim, mason, lsp)
+- tmux 
+
+![My Desktop Setup](./assets/display.png)
---
+++ nvim/init.lua
@@ -0,0 +1,166 @@
+vim.g.mapleader = " "
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  rocks = {
+    enabled = false,
+  },
+
+  {
+    "tinted-theming/base16-vim",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme base16-black-metal")
+
+      local bg_groups = { 
+        "Normal", "NormalFloat", "NormalNC", "LineNr", 
+        "SignColumn", "EndOfBuffer", "MsgArea", "Pmenu", "TelescopeBorder" 
+      }
+      for _, group in ipairs(bg_groups) do
+        vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+      end
+
+      vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#4f7c82" })
+      vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr:hor20,o:hor50"
+    end,
+  },
+
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    opts = { 
+      ensure_installed = {
+            "python",
+            "javascript",
+            "typescript",
+            "c",
+            "cpp",
+            "lua",
+      },
+      highlight = { enable = true },
+    },
+  },
+    
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("mason").setup()
+
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "ts_ls", "clangd" },
+      })
+
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("ts_ls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("clangd", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("clangd")
+
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+})
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.cursorline = true
+vim.opt.pumblend = 10
+vim.opt.winblend = 10
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.mouse = "a"
+vim.opt.lazyredraw = true
+
+local builtin = require("telescope.builtin")
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files)
+vim.keymap.set("n", "<leader>fg", builtin.live_grep)
+vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fh", builtin.help_tags)
