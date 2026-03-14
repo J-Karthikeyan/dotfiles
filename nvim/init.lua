ada3260a82d693088a1d97f0c11dbf452cf4b46e
J-Karthikeyan <jkarthik2611@gmail.com>
2026-03-14 12:47:43 +0530

    backup: pre-migration state

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
-   "--filter=blob:none",
+    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     lazypath,
   })
@@ -16,57 +16,204 @@ require("lazy").setup({
     enabled = false,
   },
 
+  -- 1. THEME & UI POLISH
   {
     "tinted-theming/base16-vim",
     priority = 1000,
     config = function()
       vim.cmd("colorscheme base16-black-metal")
 
-      -- Inside your base16-black-metal config function
+      -- Transparency and custom highlights
       local bg_groups = { "Normal", "NormalNC", "LineNr", "SignColumn", "EndOfBuffer" }
-      for _, group in ipairs(bg_groups) do
-        vim.api.nvim_set_hl(0, group, { bg = "none" })
-      end
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#191f22" }) 
-      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#7cb3ba", bg = "#191f22" })
-
       for _, group in ipairs(bg_groups) do
         vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
       end
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#191f22" })
+      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#7cb3ba", bg = "#191f22" })
 
       vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#4f7c82" })
+      vim.api.nvim_set_hl(0, "IblScope", { fg = "#4f7c82" }) -- Define missing highlight for indent-blankline
       vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-Cursor,r-cr:hor20,o:hor50"
     end,
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        theme = "auto",
+        globalstatus = true,
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+      options = {
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+      scope = { enabled = false },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.styled_parts"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("alpha").setup(require("alpha.themes.dashboard").config)
+    end,
+  },
 
+  -- 2. NAVIGATION & EXPLORATION
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      window = {
+        width = 30,
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Show hidden files
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {},
+  },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 
+  -- 3. EDITING POWER
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require("mini.pairs").setup()
+      require("mini.surround").setup()
+      require("mini.comment").setup()
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+
+  -- 4. GIT INTEGRATION
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {},
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+
+  -- 5. CODING INTELLIGENCE
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  {
+    "Bekaboo/dropbar.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    opts = {}
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     lazy = false,
-    opts = { 
+    opts = {
       ensure_installed = {
-            "python",
-            "javascript",
-            "typescript",
-            "c",
-            "cpp",
-            "lua",
-            "r",
-            "tsx",
+        "python",
+        "javascript",
+        "typescript",
+        "c",
+        "cpp",
+        "lua",
+        "r",
+        "tsx",
+        "rust",
       },
       highlight = { enable = true },
     },
   },
-    
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -76,47 +223,26 @@ require("lazy").setup({
     },
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       require("mason").setup()
-
       require("mason-lspconfig").setup({
-        ensure_installed = { "pyright", "ts_ls", "clangd", "r_language_server", "tailwindcss" },
+        ensure_installed = { "pyright", "ts_ls", "clangd", "r_language_server", "tailwindcss", "rust_analyzer" },
       })
 
-      vim.lsp.config("pyright", {
-        capabilities = capabilities,
-      })
-
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-      })
-
-      vim.lsp.config("clangd", {
-        capabilities = capabilities,
-      })
-
-      local handlers = {
-        ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-        ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signatureHelp, { border = "rounded" }),
-      }
-
-      vim.lsp.config("r_language_server", { handlers = handlers })
-      vim.lsp.config("ts_ls", { handlers = handlers })
-      vim.lsp.config("pyright", { handlers = handlers })
-
-      vim.lsp.enable("pyright")
-      vim.lsp.enable("ts_ls")
-      vim.lsp.enable("clangd")
-      vim.lsp.enable("r_language_server")
-      vim.lsp.enable("tailwindcss")
+      local servers = { "pyright", "ts_ls", "clangd", "r_language_server", "tailwindcss", "rust_analyzer" }
+      for _, server in ipairs(servers) do
+        vim.lsp.config(server, {
+          capabilities = capabilities,
+        })
+        vim.lsp.enable(server)
+      end
 
       vim.keymap.set("n", "gd", vim.lsp.buf.definition)
       vim.keymap.set("n", "K", vim.lsp.buf.hover)
       vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+      vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
     end,
   },
-
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -127,7 +253,6 @@ require("lazy").setup({
     },
     config = function()
       local cmp = require("cmp")
-
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -146,19 +271,19 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
 })
 
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_python3_provider = 0
-
+-- CORE SETTINGS
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus"
-
 vim.opt.cursorline = true
 vim.opt.pumblend = 10
 vim.opt.winblend = 10
@@ -167,17 +292,39 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-
 vim.opt.updatetime = 250
-vim.opt.timeoutlen = 200
+vim.opt.timeoutlen = 300
 vim.opt.scrolloff = 5
 vim.opt.sidescrolloff = 8
 vim.opt.mouse = "a"
-vim.opt.lazyredraw = true
 
+-- KEYMAPS
 local builtin = require("telescope.builtin")
-
 vim.keymap.set("n", "<leader>ff", builtin.find_files)
 vim.keymap.set("n", "<leader>fg", builtin.live_grep)
 vim.keymap.set("n", "<leader>fb", builtin.buffers)
 vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>") 
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>")
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>")
+
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>")
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+
+vim.keymap.set("n", "<leader>bn", "<cmd>bnext<cr>")
+vim.keymap.set("n", "<leader>bp", "<cmd>bprev<cr>")
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>")
+
+-- Disable providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
